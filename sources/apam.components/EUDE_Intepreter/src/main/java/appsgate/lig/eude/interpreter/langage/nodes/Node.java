f07b4e863807d5f181d7efd7fb071608bd16821d
Cedric <cedric.gerard@inria.fr>
2013-04-26 18:15:24 +0200

    Add node model for appsgate programming language

---
+++ sources/apam.components/EUDE_Intepreter/src/main/java/appsgate/lig/eude/interpreter/langage/nodes/Node.java
@@ -0,0 +1,21 @@
+package appsgate.lig.eude.interpreter.langage.nodes;
+
+public abstract class Node {
+	
+	//TODO add EVT_START 
+	//TODO add EVT_END
+	//TODO add SymboleTable
+	
+	//Abstract Methods
+	//TODO deploy
+	//TODO stop
+	//TODO resume
+	//TODO getState
+	
+	//Methods
+	//TODO getaprentSymboleTable
+	
+	//TODO start and end event will be manage by subscription methods.
+	//A node can subscribe itself to be call back when a child node end or start its execution.
+
+}
---
+++ sources/apam.components/EUDE_Intepreter/src/main/java/appsgate/lig/eude/interpreter/langage/nodes/NodeWhen.java
@@ -0,0 +1,5 @@
+package appsgate.lig.eude.interpreter.langage.nodes;
+
+public class NodeWhen extends Node {
+
+}
