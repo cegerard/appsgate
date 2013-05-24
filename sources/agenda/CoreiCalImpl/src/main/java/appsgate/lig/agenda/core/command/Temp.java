37fd40657d9c4088051083b2028f31f3149a4c14
jbotnascimento <jbotnascimento@gmail.com>
2013-05-24 13:20:23 +0200

    adding

---
+++ sources/agenda/CoreiCalImpl/src/main/java/appsgate/lig/agenda/core/command/Temp.java
@@ -0,0 +1,21 @@
+package appsgate.lig.agenda.core.command;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Temp {
+
+	public static void main(String[] args) throws ParseException {
+		String target = "25052013 14:00";
+		DateFormat df = new SimpleDateFormat("ddMMyyyy HH:mm");
+		df.setTimeZone(TimeZone.getDefault());
+		Date d=df.parse(target);
+		
+		System.out.println("--"+d);
+		
+	}
+	
+}
