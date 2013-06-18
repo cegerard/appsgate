4044d355edf4940afa5562f9a7d1d2661d3dc542
Remy Dautriche <remy.dautriche@caladan.fr>
2013-06-18 16:07:43 +0200

    Added EUDE_InterpreterSpec

---
+++ sources/apam.components/EUDE_InterpreterSpec/src/main/java/appsgate/lig/eude/interpreter/spec/EUDE_InterpreterSpec.java
@@ -0,0 +1,40 @@
+package appsgate.lig.eude.interpreter.spec;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
+/**
+ * Interpreter component for end-user development
+ * 
+ * @author Rémy Dautriche
+ * @version 1.0.0
+ */
+public interface EUDE_InterpreterSpec {
+
+	/**
+	 * Add a new program to the interpreter. Instantiate the program from its JSON representation
+	 * 
+	 * @param programJSON
+	 * @return
+	 */
+	public boolean addProgram(JSONObject programJSON);
+	
+	/**
+	 * Launch the interpretation of a program
+	 * 
+	 * @param programName Name of the program to launch
+	 * @return true if the program has been successfully launched, false otherwise
+	 */
+	public boolean callProgram(String programName);
+	
+	/**
+	 * Return a hash map containing all the programs known by the interpreter.
+	 * The keys are the programs' name and the value are the programs under their
+	 * JSON format
+	 * 
+	 * @return HashMap with all the existing programs
+	 */
+	public HashMap<String, JSONObject> getListPrograms();
+	
+}
