3439afa3f32fd889acc41d70f20315f3b2776967
Remy Dautriche <remy.dautriche@caladan.fr>
2013-06-19 11:41:28 +0200

    Merged

---
+++ sources/apam.components/EUDE_Intepreter/src/main/java/appsgate/lig/eude/interpreter/langage/components/EndEvent.java
@@ -0,0 +1,11 @@
+package appsgate.lig.eude.interpreter.langage.components;
+
+import java.util.EventObject;
+
+public class EndEvent extends EventObject {
+
+	public EndEvent(Object source) {
+		super(source);
+	}
+	
+}
