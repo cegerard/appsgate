6f626b5d20a3d727b7ec374402507bc67b0e1c38
jbotnascimento <jbotnascimento@gmail.com>
2013-07-16 11:14:06 +0200

    fixing polling connection exception

--- sources/mail/src/main/java/appsgate/lig/mail/Mail.java
+++ sources/mail/src/main/java/appsgate/lig/mail/Mail.java
@@ -1,5 +1,6 @@
 package appsgate.lig.mail;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.mail.Message;
@@ -17,11 +18,13 @@ public interface Mail {
 
 	public Session getSession();
 	public IMAPFolder getMailBox(String boxname) throws MessagingException;
+	public void fetch() throws MessagingException;
 	public List<Message> getMails();
 	public Message getMail(String messageId);
 	public boolean sendMailSimple(String to,String subject,String body);
 	public boolean sendMail(Message message);
 	public void addFolderListener(FolderChangeListener listener);
 	public void removeFolderListener(FolderChangeListener listener);
+	public Calendar getLastFetchDateTime();
 	
 }
