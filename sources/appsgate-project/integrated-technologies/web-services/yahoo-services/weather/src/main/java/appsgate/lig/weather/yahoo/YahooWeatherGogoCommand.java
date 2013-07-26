1f38ee176de1ccb0dd6af3d6aaa0c7a549b47646
Thibaud Flury <thibaud.flury@imag.fr>
2013-07-26 15:04:50 +0200

    splitted weather service between specs and implem, added appsgate specificities

--- sources/appsgate-project/integrated-technologies/web-services/yahoo-services/weather/src/main/java/appsgate/lig/weather/yahoo/YahooWeatherGogoCommand.java
+++ sources/appsgate-project/integrated-technologies/web-services/yahoo-services/weather/src/main/java/appsgate/lig/weather/yahoo/YahooWeatherGogoCommand.java
@@ -7,8 +7,8 @@ import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.ServiceProperty;
 
-import appsgate.lig.weather.WeatherForecast;
-import appsgate.lig.weather.WeatherForecastException;
+import appsgate.lig.weather.spec.CoreWeatherServiceSpec;
+import appsgate.lig.weather.exception.WeatherForecastException;
 import fr.imag.adele.apam.Apam;
 import fr.imag.adele.apam.CST;
 import fr.imag.adele.apam.Instance;
@@ -22,56 +22,98 @@ import fr.imag.adele.apam.Instance;
  */
 public class YahooWeatherGogoCommand {
 
-	@Requires
-	Apam apam;
-	
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.group.id", value = "meteo")
-	String universalShell_groupID;
+    @Requires
+    Apam apam;
 
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.group.name", value = "meteo commands")
-	String universalShell_groupName;
+    @ServiceProperty(name = "org.knowhowlab.osgi.shell.group.id", value = "weather")
+    String universalShell_groupID;
 
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.commands", value = "{}")
-	String[] universalShell_groupCommands = new String[] {
-			"meteoShow#show last collected meteo data",
-			"meteoFetch#show data to be fetched from the meteo provider"
-	};
+    @ServiceProperty(name = "org.knowhowlab.osgi.shell.group.name", value = "weather commands")
+    String universalShell_groupName;
 
-	public void meteoShow(PrintWriter out, String... args){
-		
-		for(Instance instance:CST.componentBroker.getInsts()){
-			
-			//Only those services that implement this spec are acceptable
-			if(!instance.getSpec().getName().equals("weather-service-specification")) continue;
-			
-			out.print(String.format("Apam-Instance: %s\n", instance.getName()));
-			
-			WeatherForecast meteo=(WeatherForecast)instance.getServiceObject();
-		
-			out.println(meteo);
-			
-			out.print(String.format("/Apam-Instance: %s \n", instance.getName()));
-			
-		}
-		
+    @ServiceProperty(name = "org.knowhowlab.osgi.shell.commands", value = "{}")
+    String[] universalShell_groupCommands = new String[] {
+	    "weatherShow#show last collected meteo data",
+	    "weatherFetch#show data to be fetched from the meteo provider",
+	    "weatherLoc#create, remove or show current locations monitored by weather service" };
+
+    public void weatherShow(PrintWriter out, String... args) {
+
+	for (Instance instance : CST.componentBroker.getInsts()) {
+
+	    // Only those services that implement this spec are acceptable
+	    if (!instance.getSpec().getName().equals("CoreWeatherServiceSpec"))
+		continue;
+
+	    out.print(String.format("Apam-Instance: %s\n", instance.getName()));
+
+	    CoreWeatherServiceSpec meteo = (CoreWeatherServiceSpec) instance
+		    .getServiceObject();
+
+	    out.println(meteo);
+
+	    out.print(String.format("/Apam-Instance: %s \n", instance.getName()));
+
 	}
-	
-	public void meteoFetch(PrintWriter out, String... args){
-	
-		for(Instance instance:CST.componentBroker.getInsts()){
-			
-			//Only those services that implement this spec are acceptable
-			if(!instance.getSpec().getName().equals("weather-service-specification")) continue;
-			
-			WeatherForecast meteo=(WeatherForecast)instance.getServiceObject();
-			try {
-			    meteo.fetch();
-			} catch(WeatherForecastException exc) {
-			    exc.printStackTrace();
-			}
-			
-		}
-		
+
+    }
+
+    public void weatherFetch(PrintWriter out, String... args) {
+
+	for (Instance instance : CST.componentBroker.getInsts()) {
+
+	    // Only those services that implement this spec are acceptable
+	    if (!instance.getSpec().getName().equals("CoreWeatherServiceSpec"))
+		continue;
+
+	    CoreWeatherServiceSpec meteo = (CoreWeatherServiceSpec) instance
+		    .getServiceObject();
+	    try {
+		meteo.fetch();
+	    } catch (WeatherForecastException exc) {
+		exc.printStackTrace();
+	    }
+
 	}
-	
+
+    }
+
+    public void weatherLoc(PrintWriter out, String... args) {
+
+	for (Instance instance : CST.componentBroker.getInsts()) {
+
+	    // Only those services that implement this spec are acceptable
+	    if (!instance.getSpec().getName().equals("CoreWeatherServiceSpec"))
+		continue;
+
+	    CoreWeatherServiceSpec meteo = (CoreWeatherServiceSpec) instance
+		    .getServiceObject();
+	    try {
+		switch (args.length) {
+		case 0:
+		    String[] locations = meteo.getLocations();
+		    if (locations != null && locations.length > 0)
+			for (String loc : locations)
+			    out.println(loc);
+		    break;
+		case 1:
+		    if (args[0].equals("--help"))
+			out.println("syntax: meteoLoc with no arguments shows all location currently monitored by weather service"
+				+ "\nsyntax: meteoLoc [location] try to add the location if not currently monitored OR remove it il already monitored");
+		    else if (meteo.containLocation(args[0]))
+			meteo.removeLocation(args[0]);
+		    else
+			meteo.addLocation(args[0]);
+		    break;
+		default:
+		    out.println("invalid number of arguments, type --help to obtain more information about the syntax");
+		}
+	    } catch (WeatherForecastException exc) {
+		exc.printStackTrace();
+	    }
+
+	}
+
+    }
+
 }
