fb818ce5e1cd3ebc8580b33d7c6eaa2fd3d8ace0
Cedric <cedric.gerard@inria.fr>
2013-09-25 15:32:10 +0200

    Core clock program integration done

--- sources/appsgate-project/appsgate-context/ContextFollower/src/main/java/appsgate/lig/context/follower/impl/ContextFollowerImpl.java
+++ sources/appsgate-project/appsgate-context/ContextFollower/src/main/java/appsgate/lig/context/follower/impl/ContextFollowerImpl.java
@@ -1,6 +1,7 @@
 package appsgate.lig.context.follower.impl;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -10,9 +11,12 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import appsgate.lig.clock.sensor.spec.AlarmEventObserver;
+import appsgate.lig.clock.sensor.spec.CoreClockSpec;
 import appsgate.lig.context.follower.listeners.CoreListener;
 import appsgate.lig.context.follower.spec.ContextFollowerSpec;
 import appsgate.lig.core.object.messages.NotificationMsg;
+import appsgate.lig.core.object.spec.CoreObjectSpec;
 
 /**
  * This class is use to allow other components to subscribe for specific
@@ -27,10 +31,22 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 	/**
 	 * Static class member uses to log what happened in each instances
 	 */
-	private static Logger logger = LoggerFactory
-			.getLogger(ContextFollowerImpl.class);
+	private static Logger logger = LoggerFactory.getLogger(ContextFollowerImpl.class);
 	
+	/**
+	 * Events subscribers list
+	 */
 	private HashMap<Entry, ArrayList<CoreListener>> eventsListeners = new HashMap<Entry, ArrayList<CoreListener>>();
+	
+	/**
+	 * Hash map use to conserve alarm identifier.
+	 */
+	private HashMap<Entry, Integer> alarmListenerList = new HashMap<Entry, Integer>();
+	
+	/**
+	 * The AppsGate time sensor 
+	 */
+	private CoreClockSpec coreClock;
 
 	/**
 	 * Called by APAM when an instance of this implementation is created
@@ -51,6 +67,20 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 		logger.debug("Adding a listener...");
 		Entry eventKey = new Entry(coreListener.getObjectId(), coreListener.getEvent(), coreListener.getValue());
 		
+		//Check if the need to by register in the core clock implementation
+		CoreObjectSpec abstractClock = (CoreObjectSpec)coreClock;
+		if(abstractClock.getAbstractObjectId().contentEquals(coreListener.getObjectId())) {
+			//Generate calendar java object for core clock
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(Long.valueOf(coreListener.getValue()));
+			//register the alarm
+			int alarmId = coreClock.registerAlarm(calendar, new TimeObserver());
+			//change the event entry with the alarmId value
+			eventKey.setValue(String.valueOf(alarmId));
+			//save the alarm identifier
+			alarmListenerList.put(eventKey, alarmId);
+		}
+		
 		Set<Entry> keys = eventsListeners.keySet();
 		Iterator<Entry> keysIt = keys.iterator();
 		boolean added = false;
@@ -70,6 +100,7 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 			eventsListeners.put(eventKey, coreListenerList);
 			logger.debug("Add new event follower list");
 		}
+		
 	}
 
 	@Override
@@ -87,6 +118,11 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 				coreListenerList.remove(coreListener);
 				if(coreListenerList.isEmpty()) {
 					eventsListeners.remove(key);
+				}
+				Integer alarmId  = alarmListenerList.get(key);
+				if(alarmId != null) {
+					coreClock.unregisterAlarm(alarmId);
+					alarmListenerList.remove(key);
 				}
 				break;
 			}
@@ -184,6 +220,10 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 		public String getValue() {
 			return value;
 		}
+		
+		public void setValue(String value) {
+			this.value = value;
+		}
 
 		/**
 		 * Override the equals method to compare two entry together but 
@@ -200,6 +240,21 @@ public class ContextFollowerImpl implements ContextFollowerSpec {
 			return false;
 		}
 
+	}
+	
+	/**
+	 * Inner class to register time notification through the core clock
+	 * @author Cédric Gérard
+	 * @since September 25, 2013
+	 * @version 1.0.0
+	 */
+	private class TimeObserver implements AlarmEventObserver {
+
+		@Override
+		public void alarmEventFired(int alarmEventId) {
+			logger.debug("Alarm notification fired: "+alarmEventId);
+		}
+		
 	}
 	
 
