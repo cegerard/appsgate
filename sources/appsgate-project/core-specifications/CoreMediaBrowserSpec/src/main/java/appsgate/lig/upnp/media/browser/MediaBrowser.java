720758d11d2e9a2ef16aa4bdcdbacb82323aa514
vega <german.vega@imag.fr>
2013-12-03 09:09:32 +0100

    Add Media Browser

---
+++ sources/appsgate-project/core-specifications/CoreMediaBrowserSpec/src/main/java/appsgate/lig/upnp/media/browser/MediaBrowser.java
@@ -0,0 +1,20 @@
+package appsgate.lig.upnp.media.browser;
+
+
+public interface MediaBrowser {	
+	
+	/**
+	 * Flag to ask only for metadata of the specified directory
+	 */
+	public static final String BROWSE_METADATA = "BrowseMetadata";
+	
+	/**
+	 * flag to ask for the list of direct children and the metadata of the specified directory
+	 */
+	public static final String BROWSE_CHILDREN = "BrowseDirectChildren";
+	
+	/**
+	 * browse the contents of the media directory identified by objectId ("0" is the root object of the media server).
+	 */
+	public String browse(String objectID,String browseFlag,String filter,long startingIndex,long requestedCount,String sortCriteria);
+}
