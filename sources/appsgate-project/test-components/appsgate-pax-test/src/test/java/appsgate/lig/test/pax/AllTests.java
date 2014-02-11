e2f73597113241aa7e959f2e3278cd4e4ad55a78
Thibad Flury <thibaud.flury@imag.fr>
2014-02-11 14:53:51 +0100

    Added tests for the web services (weather, mail, calendar, clock) -> TODO: Add tests with real account for calendar and mail, for the moment, only check if instances are created correctly

--- sources/appsgate-project/test-components/appsgate-pax-test/src/test/java/appsgate/lig/test/pax/AllTests.java
+++ sources/appsgate-project/test-components/appsgate-pax-test/src/test/java/appsgate/lig/test/pax/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestEmptyApAM.class,TestCoreAppsgate.class})
+@SuiteClasses({TestEmptyApAM.class,TestWebServicesAppsgate.class,TestCoreAppsgate.class})
 public class AllTests {
 
 }
--- sources/appsgate-project/test-components/appsgate-pax-test/src/test/java/appsgate/lig/test/pax/TestCoreAppsgate.java
+++ sources/appsgate-project/test-components/appsgate-pax-test/src/test/java/appsgate/lig/test/pax/TestCoreAppsgate.java
@@ -40,16 +40,19 @@ public class TestCoreAppsgate extends PaxedDistribution {
 	private static Logger logger = LoggerFactory.getLogger(TestCoreAppsgate.class);
 
 	
-	@Test
-	public void testEmptyAppsgate() {
+	public static void testEmptyAppsgate() {
 		logger.debug("This test as a complete provisionning of Core Appsgate Bundles"
 				+ " (without technology specific as ubikit, watteco, upnp)");
 		ApAMHelper.waitForApam(ApAMHelper.RESOLVE_TIMEOUT);
 		ApAMHelper.waitForComponentByName(null,
 				"OBRMAN-Instance", ApAMHelper.RESOLVE_TIMEOUT);
 		Assert.assertTrue(CST.componentBroker.getInst("OBRMAN-Instance")!= null);
-		
 	}	
+	
+	@Test
+	public void testEmptyAppsgateDistribution() {
+		testEmptyAppsgate();
+	}		
 
 	@Ignore
 	@Test
@@ -64,56 +67,21 @@ public class TestCoreAppsgate extends PaxedDistribution {
 		
 		//ApAMHelper.waitForIt(10000);
 	}	
-	
-	@Test
-	public void testYahooWeather() {
-		testEmptyAppsgate();
-		
-		logger.debug("This test creates and runs an instance of Yahoo Weather");
-		Implementation weatherService = (Implementation) ApAMHelper.waitForComponentByName(null,
-				"YahooWeatherImpl", ApAMHelper.RESOLVE_TIMEOUT);
 
-		Assert.assertTrue("Specification should have been retrieved", CST.componentBroker.getSpec("CoreWeatherServiceSpec")!= null);
-		Assert.assertTrue("Implementation should have been retrieved", CST.componentBroker.getImpl("YahooWeatherImpl")!= null);
-		
-		Instance inst = weatherService.createInstance(null, null);
-		Assert.assertTrue("An instance should have been created", inst!= null);
-		
-		CoreWeatherServiceSpec service= (CoreWeatherServiceSpec) inst.getServiceObject();
-		try {
-			service.addLocation("Grenoble");
-			Assert.assertTrue("Grenoble should be added",service.containLocation("Grenoble"));
-			service.fetch();
-			Assert.assertNotNull("Weather should have been retrieved",service.getCurrentWeather());
-		} catch(Exception exc) {
-			Assert.fail(exc.getMessage());
-		}
-	}	
-	
 	
 		
 	@Configuration
 	public Option[] configuration() {
 		Map<String, String> testApps = new HashMap<String, String>();
-		fillBundleList(testApps);
+		fillCoreBundleList(testApps);
 		return super.configuration(testApps);
 	}
 	
-	public void fillBundleList(Map<String, String> testApps) {
+	public static void fillCoreBundleList(Map<String, String> testApps) {
 		testApps.put("org.apache.felix.configadmin", "org.apache.felix");
 		testApps.put("org.apache.felix.fileinstall", "org.apache.felix");
 		testApps.put("org.apache.felix.http.api", "org.apache.felix");
 		testApps.put("org.apache.felix.http.jetty", "org.apache.felix");
-		
-		testApps.put("bcprov-jdk15on", "org.bouncycastle");
-		testApps.put("com.google.gdata-calendar", "org.openengsb.wrapped");
-		testApps.put("commons-logging", "commons-logging");
-		testApps.put("commons-lang3", "org.apache.commons");
-		testApps.put("org.apache.servicemix.bundles.kxml2", "org.apache.servicemix.bundles");
-		testApps.put("guava-osgi", "com.googlecode.guava-osgi");
-		testApps.put("ical4j", "org.mnode.ical4j");
-
-		
 		
 		testApps.put("json", "org.json");
 		testApps.put("mongo-java-driver", "org.mongodb");		
