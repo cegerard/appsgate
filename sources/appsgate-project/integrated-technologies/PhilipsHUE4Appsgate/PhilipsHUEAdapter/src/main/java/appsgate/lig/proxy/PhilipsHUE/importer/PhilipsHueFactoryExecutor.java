adc196deaabf7c4206cc2c33b85d2e78b3e464b3
jander nascimento <jbotnascimento@gmail.com>
2014-07-11 10:22:12 +0200

    Resolve blockage isssue (found by german, thibaud)
    Delegating apam instance creating for a thread in order not to block ipojo

---
+++ sources/appsgate-project/integrated-technologies/PhilipsHUE4Appsgate/PhilipsHUEAdapter/src/main/java/appsgate/lig/proxy/PhilipsHUE/importer/PhilipsHueFactoryExecutor.java
@@ -0,0 +1,113 @@
+package appsgate.lig.proxy.PhilipsHUE.importer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHBridgeConfiguration;
+import com.philips.lighting.model.PHLight;
+import com.philips.lighting.model.PHLightState;
+import fr.imag.adele.apam.CST;
+import fr.imag.adele.apam.Implementation;
+import fr.imag.adele.apam.Instance;
+import org.ow2.chameleon.fuchsia.core.declaration.ImportDeclaration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PhilipsHueFactoryExecutor implements Runnable {
+
+    public static String ApAMIMPL = "PhilipsHUEImpl";
+    final ImportDeclaration importDeclaration;
+    private final PHBridge bridge;
+    private final PHLight light;
+    private final PhilipsHueImporter importer;
+    private Logger LOG= LoggerFactory.getLogger(PhilipsHueFactoryExecutor.class);
+
+    public PhilipsHueFactoryExecutor(PHBridge bridge, PHLight light, PhilipsHueImporter importer, ImportDeclaration importDeclaration){
+        this.bridge=bridge;
+        this.light=light;
+        this.importer=importer;
+        this.importDeclaration=importDeclaration;
+    }
+
+    @Override
+    public void run()  {
+
+        try {
+            PHBridgeConfiguration bc  = bridge.getResourceCache().getBridgeConfiguration();
+            String deviceID = bc.getMacAddress()+"-"+light.getIdentifier();
+            Implementation impl = CST.apamResolver.findImplByName(null, ApAMIMPL);
+            Map<String, String> properties = new HashMap<String, String>();
+
+            properties.put("deviceName", 	light.getName());
+            properties.put("deviceId", 		deviceID);
+            properties.put("lightBridgeId", light.getIdentifier());
+            properties.put("lightBridgeIP", bc.getIpAddress());
+            properties.put("reachable", Boolean.toString(light.isReachable()));
+            initiateLightStateProperties(properties, light.getLastKnownLightState());
+
+            if(impl != null) {
+                try {
+                    Instance inst = impl.createInstance(null, properties);
+                }catch(Exception e){
+                    LOG.error("Failed to instantiate apam implementation {} (lamp proxy for appsgate)",e);
+                }
+            }else {
+                System.out.println("No "+ApAMIMPL+" found !");
+            }
+
+            importer.handleImportDeclaration(importDeclaration);
+
+        } catch (Exception e) {
+            LOG.error("Failed to create appsgate instance for the light.",e);
+        }
+    }
+
+    public void initiateLightStateProperties(Map<String, String> properties, PHLightState lightState) {
+        properties.put("state", String.valueOf(lightState.isOn()));
+        properties.put("hue", String.valueOf(lightState.getHue()));
+        properties.put("sat", String.valueOf(lightState.getSaturation()));
+        properties.put("bri", String.valueOf(lightState.getBrightness()));
+        properties.put("x", String.valueOf(lightState.getX()));
+        properties.put("y", String.valueOf(lightState.getY()));
+        properties.put("ct", String.valueOf(lightState.getCt()));
+        properties.put("speed", String.valueOf(lightState.getTransitionTime()));
+
+        if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_SELECT.name())) {
+            properties.put("alert", "select");
+        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_LSELECT.name())){
+            properties.put("alert", "lselect");
+        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_NONE.name())){
+            properties.put("alert", "none");
+        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_UNKNOWN.name())){
+            properties.put("alert", "unknown");
+        }else {
+            System.out.println("Error when initiating the HUE light alert value!");
+        }
+
+        if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_COLORLOOP.name())) {
+            properties.put("effect",  "colorloop");
+        }else if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_NONE.name())){
+            properties.put("effect",  "none");
+        }else if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_UNKNOWN.name())){
+            properties.put("effect",  "unknown");
+        }else {
+            System.out.println("Error when initiating the HUE light alert value!");
+        }
+
+        if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_HUE_SATURATION.name())) {
+            properties.put("mode",  "hs");
+        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_CT.name())){
+            properties.put("mode",  "ct");
+        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_XY.name())){
+            properties.put("mode",  "xy");
+        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_NONE.name())){
+            properties.put("mode",  "none");
+        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_UNKNOWN.name())){
+            properties.put("mode",  "unknown");
+        }else {
+            System.out.println("Error when initiating the HUE light alert value!");
+        }
+    }
+}
--- sources/appsgate-project/integrated-technologies/PhilipsHUE4Appsgate/PhilipsHUEAdapter/src/main/java/appsgate/lig/proxy/PhilipsHUE/importer/PhilipsHueImporter.java
+++ sources/appsgate-project/integrated-technologies/PhilipsHUE4Appsgate/PhilipsHUEAdapter/src/main/java/appsgate/lig/proxy/PhilipsHUE/importer/PhilipsHueImporter.java
@@ -1,13 +1,7 @@
 package appsgate.lig.proxy.PhilipsHUE.importer;
 
 import com.philips.lighting.hue.sdk.bridge.impl.PHBridgeImpl;
-import com.philips.lighting.model.PHBridge;
-import com.philips.lighting.model.PHBridgeConfiguration;
 import com.philips.lighting.model.PHLight;
-import com.philips.lighting.model.PHLightState;
-import fr.imag.adele.apam.CST;
-import fr.imag.adele.apam.Implementation;
-import fr.imag.adele.apam.Instance;
 import org.apache.felix.ipojo.annotations.*;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -99,93 +93,19 @@ public class PhilipsHueImporter extends AbstractImporterComponent {
 
             lamps.put(pojo.getId(),lampService);
 
-            try {
-                instanciateHUELight(bridges.get(0), (PHLight) pojo.getObject());
-                super.handleImportDeclaration(importDeclaration);
-            } catch (Exception e) {
-                LOG.error("Failed to create appsgate instance for the light.",e);
-            }
+            //This is done in a different thread duo to an Apam blockage
+            new Thread(new PhilipsHueFactoryExecutor(bridges.get(0), (PHLight) pojo.getObject(),this,importDeclaration)).start();
+
 
         } catch (ClassNotFoundException e) {
             LOG.error("Failed to load type {}, importing process aborted.", pojo.getType(), e);
         }
 
-
     }
 
-    public static String ApAMIMPL = "PhilipsHUEImpl";
 
-    public void instanciateHUELight(PHBridge bridge, PHLight light) throws Exception {
-        PHBridgeConfiguration bc  = bridge.getResourceCache().getBridgeConfiguration();
-        String deviceID = bc.getMacAddress()+"-"+light.getIdentifier();
-        Implementation impl = CST.apamResolver.findImplByName(null, ApAMIMPL);
-        Map<String, String> properties = new HashMap<String, String>();
 
-        properties.put("deviceName", 	light.getName());
-        properties.put("deviceId", 		deviceID);
-        properties.put("lightBridgeId", light.getIdentifier());
-        properties.put("lightBridgeIP", bc.getIpAddress());
-        properties.put("reachable", Boolean.toString(light.isReachable()));
-        initiateLightStateProperties(properties, light.getLastKnownLightState());
 
-        if(impl != null) {
-            try {
-                Instance inst = impl.createInstance(null, properties);
-            }catch(Exception e){
-                LOG.error("Failed to instantiate apam implementation {} (lamp proxy for appsgate)",ApAMIMPL,e);
-                throw new Exception(e);
-            }
-        }else {
-            System.out.println("No "+ApAMIMPL+" found !");
-        }
-    }
-
-    public void initiateLightStateProperties(Map<String, String> properties, PHLightState lightState) {
-        properties.put("state", String.valueOf(lightState.isOn()));
-        properties.put("hue", String.valueOf(lightState.getHue()));
-        properties.put("sat", String.valueOf(lightState.getSaturation()));
-        properties.put("bri", String.valueOf(lightState.getBrightness()));
-        properties.put("x", String.valueOf(lightState.getX()));
-        properties.put("y", String.valueOf(lightState.getY()));
-        properties.put("ct", String.valueOf(lightState.getCt()));
-        properties.put("speed", String.valueOf(lightState.getTransitionTime()));
-
-        if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_SELECT.name())) {
-            properties.put("alert", "select");
-        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_LSELECT.name())){
-            properties.put("alert", "lselect");
-        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_NONE.name())){
-            properties.put("alert", "none");
-        }else if(lightState.getAlertMode().name().contentEquals(PHLight.PHLightAlertMode.ALERT_UNKNOWN.name())){
-            properties.put("alert", "unknown");
-        }else {
-            System.out.println("Error when initiating the HUE light alert value!");
-        }
-
-        if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_COLORLOOP.name())) {
-            properties.put("effect",  "colorloop");
-        }else if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_NONE.name())){
-            properties.put("effect",  "none");
-        }else if(lightState.getEffectMode().name().contentEquals(PHLight.PHLightEffectMode.EFFECT_UNKNOWN.name())){
-            properties.put("effect",  "unknown");
-        }else {
-            System.out.println("Error when initiating the HUE light alert value!");
-        }
-
-        if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_HUE_SATURATION.name())) {
-            properties.put("mode",  "hs");
-        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_CT.name())){
-            properties.put("mode",  "ct");
-        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_XY.name())){
-            properties.put("mode",  "xy");
-        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_NONE.name())){
-            properties.put("mode",  "none");
-        }else if(lightState.getColorMode().name().contentEquals(PHLight.PHLightColorMode.COLORMODE_UNKNOWN.name())){
-            properties.put("mode",  "unknown");
-        }else {
-            System.out.println("Error when initiating the HUE light alert value!");
-        }
-    }
 
     @Override
     protected void denyImportDeclaration(final ImportDeclaration importDeclaration) throws BinderException {
