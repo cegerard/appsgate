5c3c5ffdfae7df4736bbdcc0e4c88a673ea9a94e
Thibad Flury <thibaud.flury@imag.fr>
2014-07-15 17:02:41 +0200

    Upnp is now working on the set top box

--- sources/appsgate-project/integrated-technologies/upnp-dlna/UPnPMediaPlayer/src/main/java/appsgate/lig/upnp/media/player/command/MediaPlayerShell.java
+++ sources/appsgate-project/integrated-technologies/upnp-dlna/UPnPMediaPlayer/src/main/java/appsgate/lig/upnp/media/player/command/MediaPlayerShell.java
@@ -1,12 +1,13 @@
 package appsgate.lig.upnp.media.player.command;
 
-import java.io.PrintWriter;
+import java.io.PrintStream;
 
 
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.ServiceProperty;
+import org.apache.felix.service.command.Descriptor;
 import org.osgi.service.upnp.UPnPDevice;
 
 import appsgate.lig.core.object.spec.CoreObjectSpec;
@@ -19,44 +20,45 @@ import fr.imag.adele.apam.Instance;
 
 
 /**
- * Universal shell command for the Media player 
+ * Gogo shell command for the Media player
  * @author thibaud
  *
  */
 
 @Instantiate
-@org.apache.felix.ipojo.annotations.Component(publicFactory = false, immediate = true, name = "appsgate.universal.shell")
+@org.apache.felix.ipojo.annotations.Component(publicFactory = false, immediate = true, name = "mediaplayer.gogoshell")
 @Provides(specifications = MediaPlayerShell.class)
 public class MediaPlayerShell {
     
     @Requires
     Apam apam;
 
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.group.id", value = "media")
-	String universalShell_groupID;
+	@ServiceProperty(name = "osgi.command.scope", value = "media")
+	String gogoShell_groupID;
 
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.group.name", value = "Media player commands")
-	String universalShell_groupName;
 
-	@ServiceProperty(name = "org.knowhowlab.osgi.shell.commands", value = "{}")
-	    String[] universalShell_groupCommands = new String[] {
-	    "players#list the available players",
-	    "playMedia#Play the Media URL given as parameter (served by a valid upnp media server)",
-	    "resume#resume if currently paused",
-	    "pause#Pause the currently running media",
-	    "stop#Stop the currently running media",
-	    "getVolume#Get the currently setted media volume",
-	    "setVolume#Set the volume"
+	@ServiceProperty(name = "osgi.command.function", value = "{}")
+	    String[] gogoShell_groupCommands = new String[] {
+	    "players",
+	    "playMedia)",
+	    "resume",
+	    "pause",
+	    "stop",
+	    "getVolume",
+	    "setVolume"
 	    };
 
-	private MediaPlayer retrieveMediaPlayerInstance(String player) {
+    PrintStream out = System.out;
+
+    private MediaPlayer retrieveMediaPlayerInstance(String player) {
 		Implementation implementation = CST.apamResolver.findImplByName(null,
 			"MediaPlayer");
 		return (MediaPlayer) implementation.getInst(player).getServiceObject();
 	    
 	}
 
-	public void players(PrintWriter out, String... args) {
+    @Descriptor("list the available players")
+	public void players(@Descriptor("none") String... args) {
 		Implementation implementation = CST.apamResolver.findImplByName(null,"MediaPlayer");
 		
 		StringBuilder players = new StringBuilder();
@@ -74,47 +76,53 @@ public class MediaPlayerShell {
 		System.out.println("Currently discovered players :");
 		System.out.println(players);
 	}
-	
-	public void playMedia(PrintWriter out, String... args) {
+
+    @Descriptor("Play the Media URL given as parameter (served by a valid upnp media server")
+	public void playMedia(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null && args!=null && args.length>1) {
 	    	myPlayer.play(args[1]);
 	    }
 	}
-	
-	public void resume(PrintWriter out, String... args) {
+
+    @Descriptor("resume if currently paused")
+	public void resume(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null) {
 	    	myPlayer.play();
 	    }
 		
 	}
-	
-	public void pause(PrintWriter out, String... args) {
+
+    @Descriptor("Pause the currently running media")
+	public void pause(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null) {
 	    	myPlayer.pause();
 	    }
 		
 	}
-	
-	public void stop(PrintWriter out, String... args) {
+
+    @Descriptor("Stop the currently running media")
+	public void stop(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null) {
 	    	myPlayer.stop();
 	    }
 		
 	}
-	
-	public void getVolume(PrintWriter out, String... args) {
+
+    @Descriptor("Get the currently setted media volume")
+	public void getVolume(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null) {
 	    	out.println(((CoreObjectSpec) myPlayer).getAbstractObjectId()+", current volume : "+myPlayer.getVolume());
 	    }
 		
 	}
-	
-	public void setVolume(PrintWriter out, String... args) {
+
+    @Descriptor("Set the volume")
+	public void setVolume(@Descriptor("none") String... args) {
 	    MediaPlayer myPlayer=retrieveMediaPlayerInstance(args[0]);
 	    if(myPlayer!= null && args!=null && args.length>1) {
 	    	myPlayer.setVolume(Integer.parseInt(args[1]));
