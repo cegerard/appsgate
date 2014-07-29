2eb672f14450789da21b0504a9d4e072a0876afa
Thibad Flury <thibaud.flury@imag.fr>
2014-07-29 17:02:26 +0200

    EHMI receive again commands from remote control

--- sources/appsgate-client/app/modules/models/device/device.js
+++ sources/appsgate-client/app/modules/models/device/device.js
@@ -55,7 +55,7 @@ define([
                 objectId: this.get("id"),
                 method: method,
                 args: args,
-                TARGET: "CHMI"
+                TARGET: "EHMI"
             };
 
             if (typeof callId !== "undefined") {
