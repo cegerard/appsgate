05c4bc00d5984154d138180d09f5efbc4f72786e
Thibad Flury <thibaud.flury@imag.fr>
2014-08-05 16:33:34 +0200

    added a configuration property to defines weather observer (use org.lig.appsgate.weather.locations with comma separated values in config.properties), this is NOT the good solution (as it is preferable to use the BDD and a specific widget in the client to add/remove weather locations)

--- sources/appsgate-project/integrated-technologies/web-services/WeatherObserverImpl/src/main/java/appsgate/lig/weather/extended/impl/WeatherObserverFactory.java
+++ sources/appsgate-project/integrated-technologies/web-services/WeatherObserverImpl/src/main/java/appsgate/lig/weather/extended/impl/WeatherObserverFactory.java
@@ -3,6 +3,9 @@ package appsgate.lig.weather.extended.impl;
 import appsgate.lig.weather.extended.spec.ExtendedWeatherObserver;
 import fr.imag.adele.apam.CST;
 import fr.imag.adele.apam.Implementation;
+import fr.imag.adele.apam.util.Util;
+import org.osgi.framework.BundleContext;
+import sun.jkernel.BundleCheck;
 
 import java.util.Hashtable;
 import java.util.Map;
@@ -11,6 +14,8 @@ import java.util.Map;
  * Created by thibaud on 02/07/2014.
  */
 public class WeatherObserverFactory {
+
+    public static final String LOCATIONS="org.lig.appsgate.weather.locations";
 
 
     public static ExtendedWeatherObserver createObserver(final String location) {
@@ -24,10 +29,6 @@ public class WeatherObserverFactory {
 
             WeatherObserverImpl impl = (WeatherObserverImpl) observerImpl.getApformImpl().addDiscoveredInstance(configuration).getServiceObject();
 
-//            WeatherObserverImpl impl = (WeatherObserverImpl) observerImpl.createInstance(null, new HashMap<String, String>() {{
-//                put("currentLocation", location);
-//            }}).getServiceObject();
-
             return impl;
 
         } catch( Exception exc) {
@@ -36,15 +37,18 @@ public class WeatherObserverFactory {
         }
     }
 
+    BundleContext context;
+
+    public WeatherObserverFactory(BundleContext context) {
+        this.context = context;
+    }
+
     public void start() {
 
         try {
-            System.out.println("\n\n Starting WeatherObserverFactory \n");
-
-            ExtendedWeatherObserver gre = createObserver("Grenoble");
-            ExtendedWeatherObserver nice = createObserver("Nice");
-
-            System.out.println("Temperature in " + gre.getCurrentLocation() + " is " + gre.getCurrentTemperature());
+            for(String location : Util.split(context.getProperty(WeatherObserverFactory.LOCATIONS))) {
+                createObserver(location);
+            }
         } catch (Exception exc) {
             System.out.println(" Exception occured");
             exc.printStackTrace();
