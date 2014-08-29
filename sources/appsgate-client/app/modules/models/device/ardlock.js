a7808bb8922a11d1f2f8af8e4a354d4ad4dc2b5b
jander nascimento <jbotnascimento@gmail.com>
2014-08-29 14:45:35 +0200

    Add ARD lock 'lorsque' on the language (visible in the HMI)

--- sources/appsgate-client/app/modules/models/device/ardlock.js
+++ sources/appsgate-client/app/modules/models/device/ardlock.js
@@ -14,7 +14,64 @@ define([
      */
     initialize: function() {
       ARDLock.__super__.initialize.apply(this, arguments);
-    }
+    },
+    getEvents: function() {
+          return ["isAuthorized","isNotAuthorized"];
+      },
+    getStates: function(which) {
+          switch (which) {
+              case "state":
+                  return [];//"getLastCard"
+              default:
+                  return [];
+          }
+      },
+    getKeyboardForState: function(state, which){
+      if (which !== "state") {
+          console.error('Unsupported type of state: ' + which);
+          return null;
+      }
+      var btn = jQuery.parseHTML("<button class='btn btn-default btn-keyboard specific-node' ></button>");
+      var v = this.getJSONState("mandatory");
+      switch(state) {
+          case "getLastCard":
+              $(btn).append("<span data-i18n='devices.ard.state.authorized'/>");
+              v.phrase = "devices.ard.state.opened";
+              v.name = "getLastCard";
+              $(btn).attr("json", JSON.stringify(v));
+              break;
+          default:
+              console.error("unexpected state found for Contact Sensor: " + state);
+              btn = null;
+              break;
+      }
+      return btn;
+    },
+    getKeyboardForEvent: function(evt){
+          var btn = jQuery.parseHTML("<button class='btn btn-default btn-keyboard specific-node' ></button>");
+          var v = this.getJSONEvent("mandatory");
+          switch(evt) {
+              case "isAuthorized":
+                  $(btn).append("<span data-i18n='devices.ard.event.authorized'/>");
+                  v.eventName = "authorized";
+                  v.eventValue = "true";
+                  v.phrase = "devices.ard.event.authorized";
+                  $(btn).attr("json", JSON.stringify(v));
+                  break;
+              case "isNotAuthorized":
+                  $(btn).append("<span data-i18n='devices.ard.event.non_authorized'/>");
+                  v.eventName = "authorized";
+                  v.eventValue = "false";
+                  v.phrase = "devices.ard.event.non_authorized";
+                  $(btn).attr("json", JSON.stringify(v));
+                  break;
+              default:
+                  console.error("unexpected event found for Contact Sensor: " + evt);
+                  btn = null;
+                  break;
+          }
+          return btn;
+      }
   });
   return ARDLock;
 });
