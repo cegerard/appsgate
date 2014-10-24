01e71501be71fa2a6519f038877cf77fa7e62a3c
jrcourtois <courtoij@imag.fr>
2014-10-24 15:48:23 +0200

    and stopMyself by the way

--- sources/appsgate-client/app/modules/grammar.js
+++ sources/appsgate-client/app/modules/grammar.js
@@ -126,7 +126,7 @@ define([
 		 */
 		isProgramEmpty: function (jsonObj) {
 			var s = this.parseNode(jsonObj, -1);
-			return ! /action|keepState/.test(s);
+			return ! /stopMyself|action|keepState/.test(s);
 		}
 
     });
