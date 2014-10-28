59d01c26e7b75b6596518252875ac76503eede68
BidoisMorgan <morganbidois@gmail.com>
2014-10-28 11:05:06 +0100

    Model / Vue
    Model avancé, création de quasi toutes les structures
    Vue avancée, ajout des images, groupe pour les ondes

--- sources/appsgate-client/app/modules/models/dependancy.js
+++ sources/appsgate-client/app/modules/models/dependancy.js
@@ -10,14 +10,226 @@ define([
      */
     Dependancy = Brick.extend({
         initialize: function () {
-        },
-        loadData: function(jsonData) {
-            this.set({
-                entities: jsonData.nodes
+            var self = this;
+
+            // Initialize types
+            self.set({
+                rootNode: "",
+                height: 800,
+                width: 960,
+                mapDepthNeighbors: {},
+                entitiesTypes: ["place", "program", "service", "time", "device", "selector"],
+                relationsTypes: ["reference", "isLocatedIn", "isPlanified"]
+                //                relationsTypes: ["listenTo", "actsOn", "isPlanified", "isLocatedIn", "denotes"]
             });
+
+            // listen to the event when the data are received
+            dispatcher.on("dataGraphReady", function (event) {
+                self.set({
+                    entities: event.entities,
+                    relations: event.relations,
+                    neighbors: event.neighbors,
+                    currentEntities: event.entities,
+                    currentRelations: event.relations
+                });
+
+                console.log(event.relations);
+            });
+
+            self.on("change:rootNode", function (model) {
+                model.set({
+                    mapDepthNeighbors: buildDepthNeighborsMap(model.get("rootNode"),
+                    model.get("currentEntities"))
+                });
+            });
+        },
+
+        loadData: function (jsonData) {
+            // Creation of the data structure : relations / entities / neighbors
+            var relationsNotSorted = jsonData.links,
+                entities = jsonData.nodes,
+                relations = processRelationsID(relationsNotSorted, entities),
+                neighbors = buildNeighborsMap(relations);
+
+            dispatcher.trigger("dataGraphReady", {
+                entities: entities,
+                relations: relations,
+                neighbors: neighbors
+            });
+        },
+
+        /**
+         * Method to make a map to have the node depth from a root node
+         * @param: rootNode - Root node from where the algorithm begins
+         * @param: entities - Array of entities
+         * @return: map depth:neighbors
+         */
+        buildDepthNeighborsMap: function (rootNode, entities) {
+            resetCheckMark(entities);
+
+            /*
+             * mapResult : Map which be returned. It is map of <Depth><Array of node>, which save the nodes for each depth from the nodeRoot
+             * queueNode : Queue of node, use to process over all the node one time
+             * depthChildrenQueue : Queue of {depth,children} which is used to know how many children have a depth.
+             */
+            var mapResult = [],
+                queueNode = [],
+                depthChildrendQueue = [];
+
+            // depthChildren for the nodeRoot, with a 0 depth and children to 0 to force it to be delete at the fist iteration
+            var firstDepthChildren = {
+                "depth": 0,
+                "children": 0
+            };
+
+            depthChildrendQueue.push(firstDepthChildren);
+            queueNode.push(rootNode);
+            rootNode.checkedNeighbor = true;
+
+            while (queueNode.length > 0) {
+
+                var currentNode = queueNode.shift();
+                var currentDepthChild = depthChildrendQueue[0];
+
+                // decrement the number of children with this depth, to count the current node
+                currentDepthChild.children--;
+                // if there is no other child for this depth, delete it from the queue
+                if (currentDepthChild.children <= 0) {
+                    depthChildrendQueue.shift();
+                }
+
+                // Add the node to the map, create the index if no already done
+                if (mapResult[currentDepthChild.depth]) {
+                    mapResult[currentDepthChild.depth].push(currentNode);
+                } else {
+                    mapResult[currentDepthChild.depth] = [];
+                    mapResult[currentDepthChild.depth].push(currentNode);
+                }
+
+                var nbNeighbors = 0;
+                // Add the children of this node, to nodes queue, and mark them
+                entities.forEach(function (node) {
+                    if (this.neighboring(currentNode, node) && !node.checkedNeighbor) {
+                        // node is a child because neighbor and not checked
+                        queueNode.push(node);
+                        node.checkedNeighbor = true;
+                        nbNeighbors++;
+                    }
+                });
+
+                // Create a depthChildren object for its children. When it is their turn to be processed, this object sould be also on the first of the queue of the depthChildrenQueue
+                var newDepthChildren = {
+                    "depth": currentDepthChild.depth + 1,
+                    "children": nbNeighbors
+                };
+                // Test children for the leaf cases
+                if (newDepthChildren.children > 0) {
+                    depthChildrendQueue.push(newDepthChildren);
+                }
+            }
+
+            return mapResult;
+        },
+
+        /**
+         * Return true if the entities a & b are neighbors
+         */
+        neighboring: function (a, b) {
+            var neighbors = this.get("neighbors");
+            return neighbors[a.id + "," + b.id] || neighbors[b.id + "," + a.id];
         }
+
     });
 
-    // Return the reference to the Dependancies constructor
+    /**
+     * Method to build the map of the neighbor, ie : [id1:id2] -> 1, means entity id1 & id2 neighbors
+     * @param: relations - The array of relation on which we base to make the map
+     */
+    function buildNeighborsMap(relations) {
+        var neighbors = {};
+        relations.forEach(function (d) {
+            neighbors[d.source.id + "," + d.target.id] = 1;
+        });
+
+        return neighbors;
+    }
+
+    /**
+     * Method to process relations and make an array of relations with a reference of entities. Done the first time of loading data. After this, the process of relations will be differents.
+     * @param: relationsNoSorted - array of relations not processed
+     * @param: entities - array of entities needed to process the relations
+     * @return: Array of relations processed with Object replacing the id
+     */
+    function processRelationsID(relationsNoSorted, entities) {
+        var relations = [];
+        // Création du tableau des liens à partir de celui des nodes, en utilisant les objets comme source/target des liens et non les index comme c'est le cas par defaut pour la librairie
+        relationsNoSorted.forEach(function (e) {
+            var sourceNode = entities.filter(function (n) {
+                    return n.id === e.source;
+                })[0],
+                targetNode = entities.filter(function (n) {
+                    return n.id === e.target;
+                })[0];
+
+            // !!!!!!!!! On ne met pas une relation si une des entités de la relation est indéfinie, donc potentiellement on enlève de l'info
+            if (typeof sourceNode !== 'undefined' && typeof targetNode !== 'undefined') {
+                relations.push({
+                    source: sourceNode,
+                    target: targetNode,
+                    type: e.type
+                });
+            }
+
+        });
+
+        // Traitement pour pouvoir avoir plusieurs liens orienté pareil entre deux entités
+        //sort links by source, then target
+        relations.sort(function (a, b) {
+            if (a.source.id > b.source.id) {
+                return 1;
+            } else if (a.source.id < b.source.id) {
+                return -1;
+            } else {
+                if (a.target.id > b.target.id) {
+                    return 1;
+                }
+                if (a.target.id < b.target.id) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        //any links with duplicate source and target get an incremented 'linknum'
+        for (var i = 0; i < relations.length; i++) {
+            if (i != 0 &&
+                relations[i].source == relations[i - 1].source &&
+                relations[i].target == relations[i - 1].target) {
+                relations[i].linknum = relations[i - 1].linknum + 1;
+            } else {
+                relations[i].linknum = 1;
+            };
+        };
+
+        // Mise à jour de la map qui nous donne les voisins : linkedByIndex
+        //        buildNeighborsMap(relations);
+        // Mise à jour de la map qui nous donne les voisins du noeud racine par rapport à leur profondeur
+        //        mapNeighborDeepth = makeDeepthNeighborsMapBreadthFirst(nodeRoot);
+
+        //        console.log("nodesArray : %o", nodesArray);
+        //        console.log("linksArray : %o", linksArray);
+        return relations;
+    };
+
+    /*
+     * Reset the check mark for all the node array (marks used to set the deepth of the nodes)
+     */
+    function resetCheckMark(entities) {
+        entities.forEach(function (node) {
+            node.checkedNeighbor = false;
+        });
+    };
+
+    // Return the reference to the Dependancy constructor
     return Dependancy;
 });
--- sources/appsgate-client/app/modules/views/dependancy/graph.js
+++ sources/appsgate-client/app/modules/views/dependancy/graph.js
@@ -7,9 +7,7 @@ define([
     GraphView = Backbone.View.extend({
         template: _.template(GraphTemplate),
 
-        initialize: function () {
-            var svg, force;
-        },
+        initialize: function () {},
 
         render: function () {
             // render the editor with the program
@@ -17,21 +15,48 @@ define([
                 dependancy: this.model,
             }));
 
-            var height = 800,
-                width = 960;
+            var width = this.model.get("width"),
+                height = this.model.get("height");
 
+            // Add the svg to html
             svg = d3.select("#graph").append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
+            // Add the div for links & nodes
+            svg.append("svg:g").attr("id", "groupLink");
             svg.append("svg:g").attr("id", "groupNode");
 
+            // Build the directional arrows for the links/edges
+            // Per-type markers, as they don't inherit styles.
+            svg.append("svg:defs").selectAll("marker")
+                .data(["targeting", "reference", "isLocatedIn", "isPlanified"])
+                .enter().append("svg:marker")
+                .attr("id", String)
+                .attr("viewBox", "0 -5 10 10")
+                .attr("refX", function (t) {
+                    if (t === "targeting") {
+                        return 38
+                    } else {
+                        return 28;
+                    }
+                })
+                .attr("refY", -2)
+                .attr("markerWidth", 6)
+                .attr("markerHeight", 6)
+                .attr("orient", "auto")
+                .append("svg:path")
+                .attr("d", "M0,-5L10,0L0,5");
+
+            // Creation of Force
             force = d3.layout.force()
                 .size([width, height])
+                .linkDistance(200)
                 .gravity(0.03)
                 .on("tick", tick);
 
-            update(this.model.get("entities"), this.model);
+            // Call update to update the state of the force (node/link)
+            update(this.model);
 
             force.start();
 
@@ -40,19 +65,25 @@ define([
     });
 
 
-    function update(nodes, model) {
-        force.nodes(nodes);
+    function update(model) {
+        force.nodes(model.get("currentEntities"));
+        force.links(model.get("currentRelations"));
 
-        node = svg.select("#groupNode").selectAll(".ANODE").data(nodes, function (d) {
+
+        /******* NODES (=Entities) *******/
+
+        // Bind node to the currentEntities, a node is represented by her ID 
+        node = svg.select("#groupNode").selectAll(".nodeGroup").data(force.nodes(), function (d) {
             return d.id;
         });
 
-        nEnter = node.enter().append("svg:g")
-            .attr("class", "ANODE")
+        // New nodes
+        var nEnter = node.enter().append("svg:g")
+            .attr("class", "nodeGroup")
             .call(force.drag)
             .each(function (a) {
-                d3.select(this)
-                    .append("circle")
+                // CIRCLE
+                d3.select(this).append("circle")
                     .attr("class", "circleNode")
                     .attr("cx", function (m) {
                         return 0
@@ -61,28 +92,115 @@ define([
                         return 0
                     })
                     .attr("r", 0);
-                //                .style("fill", function (n) {
-                //                    if (n.type === "device") {
-                //                        return "red";
-                //                    }
-                //                    if (n.type === "place") {
-                //                        return "yellow";
-                //                    }
-                //                    if (n.type === "program") {
-                //                        return "blue";
-                //                    }
-                //                    return "black";
-                //                })
-                //                .transition().duration(700).attr("r", 14);
-                //                .attr("r", 14);
+                // IMAGE
+                d3.select(this).append("image")
+                    .attr("xlink:href", function (d) {
+                        var imgNode = "";
+                        if (d.type === "device") {
+                            imgNode = "/app/img/home/device3.svg";
+                        } else if (d.type === "time") {
+                            imgNode = "/app/img/home/calendar.svg";
+                        } else if (d.type === "place") {
+                            imgNode = "/app/img/home/place1.svg";
+                        } else if (d.type === "program") {
+                            imgNode = "/app/img/home/program2.svg";
+                        } else if (d.type === "service") {
+                            imgNode = "/app/img/home/service1.svg";
+                        }
+                        return imgNode;
+                    })
+                    .attr('x', -12)
+                    .attr('y', -12)
+                    .attr('width', 24)
+                    .attr('height', 24);
+                // TEXT
+                d3.select(this).append("text")
+                    .attr("class", "label")
+                    .text(function (d) {
+                        return d.name;
+                    })
+//                    .classed("nodeRoot", function (d) {
+//                        return d === nodeRoot;
+//                    })
+//                    .classed("node-0", function (d) {
+//                        return d === nodeRoot;
+//                    })
+//                    .classed("node-1", function (d) {
+//                        return neighboring(d, nodeRoot);
+//                    })
+//                    .classed("node-2", function (d) {
+//                        return getDeepthNeighbor(d, mapNeighborDeepth) === 2;
+//                    })
+//                    .classed("node-more", function (d) {
+//                        return getDeepthNeighbor(d, mapNeighborDeepth) > 2 || getDeepthNeighbor(d, mapNeighborDeepth) === -1;
+//                    });
             });
+        //            .each(function (a) {
+        //                d3.select(this)
+        //                    .append("circle")
+        //                    .attr("class", "circleNode")
+        //                    .attr("cx", function (m) {
+        //                        return 0
+        //                    })
+        //                    .attr("cy", function (m) {
+        //                        return 0
+        //                    })
+        //                    .attr("r", 0);
+        //            });
 
         nEnter.select("circle")
-            .transition().duration(800).style("fill", "red")
+            .transition().duration(800).style("fill", function (n) {
+                if (n.type === "device") {
+                    return "red";
+                }
+                if (n.type === "place") {
+                    return "yellow";
+                }
+                if (n.type === "program") {
+                    return "blue";
+                }
+                return "black";
+            })
             .transition().duration(800).attr("r", 14);
 
         node.exit().select("circle").transition().duration(800).attr("r", 0);
         node.exit().transition().duration(800).remove();
+
+
+        /******* LINKS (=Relations) *******/
+
+        pathLink = svg.select("#groupLink").selectAll(".linkG")
+            .data(force.links(), function (d) {
+                return d.source.id + "-" + d.target.id;
+            });
+
+        pathLink
+            .enter().append("svg:g")
+            .attr("class", "linkG")
+            .each(function (l, i) {
+                d3.select(this).append("svg:path")
+                    .attr("class", function (d) {
+                        return "link " + d.type;
+                    })
+                    .attr("marker-end", function (d) {
+                        return "url(#" + d.type + ")";
+                    })
+                    .attr("id", function (d) {
+                        return "linkID_" + i;
+                    })
+                    .attr("refX", -30);
+
+
+                d3.select(this).append("text")
+                    .attr("class", "linklabel")
+                    .style("font-size ", "13px ")
+                    .attr("x", "50")
+                    .attr("y", "20 ")
+                    .attr("text-anchor", "middle ")
+            })
+
+
+        pathLink.exit().remove();
     }
 
 
@@ -91,6 +209,14 @@ define([
         node
             .attr("transform", function (d) {
                 return "translate(" + (d.x) + "," + (d.y) + ")";
+            })
+
+        pathLink.select("path")
+            .attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = 150 / d.linknum; //linknum is defined above
+                return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
             })
     }
 
