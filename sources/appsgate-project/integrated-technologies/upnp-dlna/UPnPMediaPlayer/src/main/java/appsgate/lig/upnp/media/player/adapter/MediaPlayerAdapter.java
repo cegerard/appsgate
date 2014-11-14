5c932b1c7107a354c7c85df05e22cdf55b0d266b
jrcourtois <courtoij@imag.fr>
2014-11-14 14:55:02 +0100

    Revert "Merge commit 'e53558d5d6172beb49fc92a856aaef66037ba98e' into local"
    This reverts commit 739cfc25d2e229f069eaf24cd4f00273cfb78641, reversing
    changes made to 7ff202b1c07b6213aad8d64ac2e691b7578b5b4a.

--- sources/appsgate-project/integrated-technologies/upnp-dlna/UPnPMediaPlayer/src/main/java/appsgate/lig/upnp/media/player/adapter/MediaPlayerAdapter.java
+++ sources/appsgate-project/integrated-technologies/upnp-dlna/UPnPMediaPlayer/src/main/java/appsgate/lig/upnp/media/player/adapter/MediaPlayerAdapter.java
@@ -310,10 +310,8 @@ public class MediaPlayerAdapter extends CoreObjectBehavior implements MediaPlaye
 			String newVolume = node.getAttributes().getNamedItem(VAL).getNodeValue();
 			logger.trace("checkChangeStateEvent(...), new Volume = "+newVolume);
 
-			if(newVolume != currentVolume) {
-				stateChanged("volume", currentVolume, newVolume);
-				currentVolume = newVolume;
-			}
+			stateChanged("volume", currentVolume, newVolume);
+			currentVolume = newVolume;
 
 		} catch (Exception exc) {
 			logger.debug("Cannot parse volume event : "+exc.getMessage());
@@ -336,10 +334,8 @@ public class MediaPlayerAdapter extends CoreObjectBehavior implements MediaPlaye
 			String newStatus = node.getAttributes().getNamedItem(VAL).getNodeValue();
 			logger.trace("checkChangeStateEvent(...), new Status = "+newStatus);
 
-			if(newStatus != currentStatus) {
-				stateChanged("playerStatus", currentStatus, newStatus);
-				currentStatus = newStatus;
-			}
+			stateChanged("playerStatus", currentStatus, newStatus);
+			currentStatus = newStatus;
 
 		} catch (Exception exc) {
 			logger.debug("Cannot parse status event : "+exc.getMessage());
