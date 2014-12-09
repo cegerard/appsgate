1de10baae4ed8d9cf351d0ac3c0a22daa0a7250c
BidoisMorgan <morganbidois@gmail.com>
2014-12-09 15:55:25 +0100

    Modification router.debugger pour prochain filtre

--- sources/appsgate-client/app/modules/routers/debugger.js
+++ sources/appsgate-client/app/modules/routers/debugger.js
@@ -1,53 +1,104 @@
 define([
     "app",
     "views/debugger/default"
-], function(App, DebuggerView) {
+], function (App, DebuggerView) {
 
-    var DebuggerRouter = {};
-    // router
-    DebuggerRouter = Backbone.Router.extend({
-        routes: {
-            "debugger": "all",
-            "debugger/all": "all"
-        },
-        // debug everything
-        all: function() {
-            // remove and unbind the current view for the menu
-            if (appRouter.currentMenuView) {
-                appRouter.currentMenuView.close();
-                appRouter.currentMenuView = null;
-            }
-            if (appRouter.currentView) {
-                appRouter.currentView.close();
-                appRouter.currentView = null;
-            }
+	var DebuggerRouter = {};
+	// router
+	DebuggerRouter = Backbone.Router.extend({
+		routes: {
+			"debugger": "all",
+			"debugger/all": "all",
+			"debugger/:id": "filtered"
+		},
+		// debug everything
+		all: function () {
+			// remove and unbind the current view for the menu
+			if (appRouter.currentMenuView) {
+				appRouter.currentMenuView.close();
+				appRouter.currentMenuView = null;
+			}
+			if (appRouter.currentView) {
+				appRouter.currentView.close();
+				appRouter.currentView = null;
+			}
 
-            $("#main").html(appRouter.navbartemplate());
+			$("#main").html(appRouter.navbartemplate());
 
-            appRouter.navigate("#debugger/all", {replace:true});
-            $(".nav-item").removeClass("active");
-            $(".home-nav").addClass("active");
+			appRouter.navigate("#debugger/all", {
+				replace: true
+			});
+			$(".nav-item").removeClass("active");
+			$(".home-nav").addClass("active");
 
-            appRouter.currentView = new DebuggerView({el:$("#main")});
-            appRouter.currentView.render();
+			appRouter.currentView = new DebuggerView({
+				el: $("#main")
+			});
+			appRouter.currentView.render();
 
-            $("#main").append(appRouter.circlemenutemplate());
+			$("#main").append(appRouter.circlemenutemplate());
 
-            // initialize the circle menu
-            $(".controlmenu").circleMenu({
-                trigger: "click",
-                item_diameter: 50,
-                circle_radius: 75,
-                direction: 'top-right'
-            });
+			// initialize the circle menu
+			$(".controlmenu").circleMenu({
+				trigger: "click",
+				item_diameter: 50,
+				circle_radius: 75,
+				direction: 'top-right'
+			});
 
-            // resize the menu
-            appRouter.currentView.resize($(".div-scrollable"));
+			// resize the menu
+			appRouter.currentView.resize($(".div-scrollable"));
 
-            $(document).i18n();
+			$(document).i18n();
 
-            dispatcher.trigger("router:loaded");
-        }
-    });
-    return DebuggerRouter;
+			dispatcher.trigger("router:loaded");
+		},
+
+		filtered: function (id) {
+			// remove and unbind the current view for the menu
+			if (appRouter.currentMenuView) {
+				appRouter.currentMenuView.close();
+				appRouter.currentMenuView = null;
+			}
+			if (appRouter.currentView) {
+				appRouter.currentView.close();
+				appRouter.currentView = null;
+			}
+
+			$("#main").html(appRouter.navbartemplate());
+
+			appRouter.navigate("#debugger/" + id, {
+				replace: true
+			});
+			$(".nav-item").removeClass("active");
+			$(".home-nav").addClass("active");
+
+			appRouter.currentView = new DebuggerView({
+				el: $("#main")
+			});
+
+			/* Faire quelque chose avant d'afficher la vue */
+			console.log("Id entity to show : " + id);
+
+			appRouter.currentView.render();
+
+			$("#main").append(appRouter.circlemenutemplate());
+
+			// initialize the circle menu
+			$(".controlmenu").circleMenu({
+				trigger: "click",
+				item_diameter: 50,
+				circle_radius: 75,
+				direction: 'top-right'
+			});
+
+			// resize the menu
+			appRouter.currentView.resize($(".div-scrollable"));
+
+			$(document).i18n();
+
+			dispatcher.trigger("router:loaded");
+		}
+	});
+	return DebuggerRouter;
 });
