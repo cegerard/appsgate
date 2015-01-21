32409d712ec040be9a664015604cef9658c067d7
Thibad Flury <thibaud.flury@imag.fr>
2015-01-21 18:05:58 +0100

    updated specs of Pace TV Service

--- sources/appsgate-project/core-specifications/CoreTVSpec/src/main/java/appsgate/lig/tv/spec/CoreTVSpec.java
+++ sources/appsgate-project/core-specifications/CoreTVSpec/src/main/java/appsgate/lig/tv/spec/CoreTVSpec.java
@@ -9,14 +9,53 @@ package appsgate.lig.tv.spec;
  */
 public interface CoreTVSpec {
 	
+	/**
+	 * KeyCodes for the radiocontrol
+	 * (these value are used as String litterals)
+	 */
+	public enum KeyCode {
+		VK_CHANNEL_DOWN,
+		VK_CHANNEL_UP,
+		VK_MENU,
+		VK_GUIDE,
+		VK_INFO,
+		VK_UP,
+		VK_DOWN,
+		VK_LEFT,
+		VK_RIGHT, 
+		VK_SELECT,
+		VK_LAST,
+		VK_COLORED_KEY_0,// (red)
+		VK_COLORED_KEY_1,// (green)
+		VK_COLORED_KEY_2,// (blue)
+		VK_COLORED_KEY_3,// (yellow)
+		VK_0,
+		VK_1,
+		VK_2,
+		VK_3,
+		VK_4,
+		VK_5,
+		VK_6,
+		VK_7,
+		VK_8,
+		VK_9,
+		VK_EXIT, 
+		VK_ENTER
+	};
+	
+	/**
+	 * command: rc, send a radiocontrol command (see enum KeyCode for available commands)
+	 */
+	public void sendRCCommand(String keycode);
 	
 	/**
 	 * command: notify, notify a message on the STB
 	 * @param id  0/1 screen identification
 	 * @param sender URL encoded string of the sender name
-	 * @param message URL encoded notification message 
+	 * @param message URL encoded notification message
+	 * @parm
 	 */
-	public void notify(int id, String sender, String message);
+	public void notify(int id, String sender, String message, boolean ack, int duration, String icon);
 	
 	/**
 	 * command: channelUp, switch to the next channel
