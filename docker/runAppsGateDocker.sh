cbf6eb5ad29ca2bfecf5b493bb5c547832ed9d2c
Cédric Gérard <git@eriatand.org>
2015-02-11 01:10:59 +0100

    update docker command to run appsgate container

--- docker/runAppsGateDocker.sh
+++ docker/runAppsGateDocker.sh
@@ -3,5 +3,5 @@
 # Run mongo container
 docker run -d --name mongoDB mongo
 # Run appsgate container export ports and link with mongoDB
-docker run -d -p 8181:8181 -p 8087:8087 --link mongoDB:DB --name appsgate yeastlab/appsgate:master
+docker run -d -i -p 8181:8181 -p 8087:8087 --link mongoDB:DB --name appsgate yeastlab/appsgate:master
 
