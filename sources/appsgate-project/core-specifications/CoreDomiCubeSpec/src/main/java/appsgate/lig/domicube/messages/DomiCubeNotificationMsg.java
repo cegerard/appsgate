e6c86cb0e71243b1ad3c54da619533c08c56a130
Thibad Flury <thibaud.flury@imag.fr>
2015-04-03 16:48:36 +0200

    Fix #531 Domcube events works properly

--- sources/appsgate-project/core-specifications/CoreDomiCubeSpec/src/main/java/appsgate/lig/domicube/messages/DomiCubeNotificationMsg.java
+++ sources/appsgate-project/core-specifications/CoreDomiCubeSpec/src/main/java/appsgate/lig/domicube/messages/DomiCubeNotificationMsg.java
@@ -11,62 +11,9 @@ import appsgate.lig.core.object.spec.CoreObjectSpec;
  *
  */
 public class DomiCubeNotificationMsg extends CoreNotificationMsg {
-	
-	/**
-	 * The current DomiCube status
-	 */
-	private final int currentFaceNumber=0;
-	
-	/**
-	 * The current battery level
-	 */
-	private final int batteryLevel=0;
-	
-	/**
-	 * The current angle of the DomiCube
-	 */
-	private final float currentDimValue=0;
-
 
     public DomiCubeNotificationMsg(String varName, String oldValue, String newValue, String source) {
         super(varName, oldValue, newValue , source);
-    }
-	
-	/**
-	 * Get the current face number of the DomiCube instance
-	 * @return the current face number as an integer
-	 */
-	public int getCurrentFaceNumber() {
-		return currentFaceNumber;
-	}
-	
-	/**
-	 * Get the battery level
-	 * @return the battery level as an integer
-	 */
-	public int getBatteryLevel() {
-		return batteryLevel;
-	}
-
-	/**
-	 * Get the current angle of the DomiCube
-	 * @return the angle as a float
-	 */
-	public float getCurrentDimValue() {
-		return currentDimValue;
-	}
-
-	@Override
-    public String getNewValue() {
-		if(getVarName().contentEquals("newFace")) {
-			return String.valueOf(currentFaceNumber);
-		}else if(getVarName().contentEquals("newBatteryLevel")) {
-			return String.valueOf(batteryLevel);
-		}else if(getVarName().contentEquals("newDimValue")) {
-			return String.valueOf(currentDimValue);
-		}
-        
-		return null;
     }
 
 }
