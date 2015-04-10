758fe736954c3146c9b7d812f5ac66ae59f30b79
jrcourtois <courtoij@imag.fr>
2015-04-10 14:41:14 +0200

    cleaning code

--- sources/appsgate-project/communication-components/TraceMan/src/main/java/appsgate/lig/ehmi/trace/TraceMan.java
+++ sources/appsgate-project/communication-components/TraceMan/src/main/java/appsgate/lig/ehmi/trace/TraceMan.java
@@ -252,25 +252,7 @@ public class TraceMan implements TraceManSpec {
             LOGGER.debug("This command [{}] to {} from [{}] does not generate a trace", command, objectID, caller);
         }    	
     }
-
     
-    @Override
-    public synchronized void commandHasBeenPassed(String objectID, String command, String caller, ArrayList<Object> args, long timeStamp) {
-        //if the equipment has been instantiated from ApAM spec before
-        GrammarDescription grammar = getGrammar(objectID);
-        if (grammar != null && grammar.generateTrace()) {
-            JSONObject jsonDecoration = Trace.getJSONDecoration(Trace.DECORATION_TYPE.access, "write", caller, timeStamp, null, objectID, null, this.getDeviceName(objectID),
-                    grammar.getTraceMessageFromCommand(command), grammar.getContextFromParams(command, args));
-            JSONObject deviceJson = Trace.getJSONDevice(objectID, null, jsonDecoration, grammar, this);
-            //Create the notification JSON object
-            JSONObject coreNotif = Trace.getCoreNotif(deviceJson, null);
-            //Trace the notification JSON object in the trace file
-            trace(coreNotif, timeStamp);
-        } else {
-            LOGGER.debug("This command [{}] to {} from [{}] does not generate a trace", command, objectID, caller);
-        }
-    }
-
     @Override
     public synchronized void coreEventNotify(long timeStamp, String srcId, String varName, String value) {
         LOGGER.debug("coreEventNotify(long timeStamp : {}, String srcId :{}, String varName : {}, String value : {})", timeStamp, srcId, varName, value);
