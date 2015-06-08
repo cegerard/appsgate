919c3691df6dda1e7267e0412e37ccf0f75f07bd
BidoisMorgan <morganbidois@gmail.com>
2015-06-08 16:22:01 +0200

    Graph : Collection Comment & Clean

--- sources/appsgate-client/app/modules/collections/dependancies.js
+++ sources/appsgate-client/app/modules/collections/dependancies.js
@@ -10,7 +10,9 @@ define([
 		initialize: function () {
 			var self = this;
 
+			// Variable to notify refresh of the graph. It will lead to the creation of a new dependency obj
 			self.isRefresh = true;
+			// Variable to notify updating state
 			self.isUpdating = false;
 
 			// listen to the event when the graph is loaded
@@ -29,57 +31,6 @@ define([
 				handleGraph(self, graph);
 			});
 
-//			self.differencies = (function () {
-//				this.relations = [];
-//				this.entities = [];
-//
-//				this.mergeRelations = function (newRelations) {
-//					_.forEach(newRelations, function (newR) {
-//						var isUpdated = false;
-//						_.forEach(relations, function (oldR) {
-//							if (newR.target.id === oldR.target.id && newR.source.id === oldR.source.id) {
-//								oldR = newR;
-//								isUpdated = true;
-//							}
-//						});
-//						if (!isUpdated) {
-//							relations.push(newR);
-//						}
-//					});
-//				};
-//
-//				this.mergeEntities = function (newEntities) {
-//					_.forEach(newEntities, function (newE) {
-//						var entityAlreadyWaiting = _.find(entities, function (e) {
-//							return e.id === newE.id;
-//						});
-//						if (entityAlreadyWaiting) {
-//							entityAlreadyWaiting = newE;
-//						} else {
-//							entities.push(newE);
-//						}
-//					});
-//				};
-//
-//				return {
-//					pushRelations: function (newRelations) {
-//						mergeRelations(newRelations);
-//					},
-//					pushEntities: function (newEntities) {
-//						mergeEntities(newEntities);
-//					},
-//					pushDifferencies: function (newDifferencies) {
-//						mergeEntities(newDifferencies.get("entities"));
-//						mergeRelations(newDifferencies.get("relations"));
-//					},
-//					getRelationsWaiting: function () {
-//						return relations;
-//					},
-//					getEntitiesWaiting: function () {
-//						return entities;
-//					}
-//				};
-//			})();
 		},
 
 		/**
@@ -89,15 +40,26 @@ define([
 			this.isRefresh = true;
 		},
 
+		/**
+		 * Method to update the dependency. It will call server method to have the last value of the graph.
+		 * @param buildGraph : Boolean to know if the graph needs to be rebuilt or not. It has to be rebuild for some modifications (ie add/remove entity)
+		 */
 		updateDependency: function (buildGraph) {
 			var self = this;
 
+			/*
+			* It is possible that more than one event updateGraph are fired in same moment. So if we receive an updateGraph when we were currently in updating, we add a callback to catch the end of the first updateGraphFinished, and we call another update. Don't forget to remove this listener in this case. Like that, at the end of process we have the last state of the graph and not a intermediate state.
+			*/
+			
 			if (self.isUpdating) {
 				self.listenTo(dispatcher, "UpdateGraphFinished", function () {
 					// Send the request to the server to get the graph
 					communicator.sendMessage({
 						method: "getGraph",
-						args: [{type:"Boolean", value: buildGraph}],
+						args: [{
+							type: "Boolean",
+							value: buildGraph
+						}],
 						callId: "loadGraph",
 						TARGET: "EHMI"
 					});
@@ -109,7 +71,10 @@ define([
 				// Send the request to the server to get the graph
 				communicator.sendMessage({
 					method: "getGraph",
-					args: [{type:"Boolean", value: buildGraph}],
+					args: [{
+						type: "Boolean",
+						value: buildGraph
+					}],
 					callId: "loadGraph",
 					TARGET: "EHMI"
 				});
@@ -119,31 +84,31 @@ define([
 
 		}
 	});
+
 	function handleGraph(self, graph) {
-				if (!self.isRefresh) {
+		if (!self.isRefresh) {
 
-					var currentDependency = self.at(0);
-					currentDependency.updateModel(graph);
-					console.log("Dependence 0 apres modif %o", currentDependency);
-					//					self.isUpdate = false;
-					dispatcher.trigger("UpdateGraphFinished");
-					//
-				} else {
-					// Create new dependancy object
-					var newDependancy = new Dependancy();
-					newDependancy.loadData(graph);
+			var currentDependency = self.at(0);
+			currentDependency.updateModel(graph);
+			console.log("Dependence 0 apres modif %o", currentDependency);
+			dispatcher.trigger("UpdateGraphFinished");
+			//
+		} else {
+			// Create new dependancy object
+			var newDependancy = new Dependancy();
+			newDependancy.loadData(graph);
 
-					if (self.length > 0) {
-						// If there was already a model, detachEvent to be sure to have no event attached on this old model
-						self.at(0).detachEvents();
-					}
+			if (self.length > 0) {
+				// If there was already a model, detachEvent to be sure to have no event attached on this old model
+				self.at(0).detachEvents();
+			}
 
-					// Replace the existing dependancy if it already exists or just add it
-					self.reset(newDependancy);
-					self.isRefresh = false;
-					dispatcher.trigger("dependanciesReady");
-					dispatcher.trigger("router:loaded");
-				}
+			// Replace the existing dependancy if it already exists or just add it
+			self.reset(newDependancy);
+			self.isRefresh = false;
+			dispatcher.trigger("dependanciesReady");
+			dispatcher.trigger("router:loaded");
+		}
 
 	}
 	return Dependancies;
